0d123133b49a3a7c429ebe33b88abdb875ca42e2
Muthyender <n.muthyender99@gmail.com>
2022-01-30 23:12:24 +0530

    Vertical Slider

---
+++ Assignment/DiceRoll/DiceRoll.js
@@ -0,0 +1,8 @@
+let submit = document.getElementById('submit')
+let playerCount = document.getElementById('playerCount')
+let gridSize = document.getElementById('gridSize')
+
+submit.addEventListener('click', function()
+{
+    
+})
