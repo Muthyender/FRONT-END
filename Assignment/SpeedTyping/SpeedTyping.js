f97ae57a958d48ed1f32eb39c9819d10a17dc33d
Muthyender <n.muthyender99@gmail.com>
2022-02-11 14:38:51 +0530

    Speed Typing

--- Assignment/SpeedTyping/SpeedTyping.js
+++ Assignment/SpeedTyping/SpeedTyping.js
@@ -3,80 +3,76 @@ let quote = document.getElementById('quote')
 let input = document.getElementById('input')
 
 let apiQuotes = []
-let time
-// timerEl.innerText = time
+let time = 0
+let random
 
-async function getQuotes()
-{
+async function getQuotes() {
     let apiURL = "http://api.quotable.io/random"
 
     let response = await fetch(apiURL)
     apiQuotes = await response.json()
 
-    // console.log(apiQuotes.content)
-    newQuote()  
+    newQuote()
 }
 
-function newQuote()
+function newQuote() 
 {
-    let random = apiQuotes.content
-
-    quote.innerText = random
+    quote.innerText = ''
+    
+    random = apiQuotes.content
     console.log(random)
 
     let quoteArr = random.split('')
-    console.log(quote)
 
-    // quote.innerHTML = `<span id='test' style='color:red;'>${quote.innerHTML.charAt(0)}</span>`+ quote.innerHTML.slice(1);
+    quoteArr.forEach(character => 
+        {
+        let charSpan = document.createElement('span')
+        charSpan.innerText = character
+        quote.appendChild(charSpan)
+        }) 
+        input.value = ''
+}
 
-    // quote.innerHTML = quote.innerHTML.replace('a', 'a'.style.color = 'red')
-    // let original = quote.innerHTML
-    // let newOne = original.replace(original.charAt(0), `<span id='test' style='color:red;'>${original.charAt(0)} </span>`)
-    // quote.innerHTML = newOne
-
-
-    // quote.style.color = 'green'
-
-    input.addEventListener('input', () =>
+input.addEventListener('input', () => 
     {
         let enteredArr = input.value.split('')
-        
-        //     console.log(quote.innerHTML.charAt(3))
+        let allcharSpans = quote.querySelectorAll('span')
+        allcharSpans.forEach((eachSpan, index) => 
+        {
+            if (!enteredArr[index]) 
+            {
+                allcharSpans[index].classList.remove('correct')
+                allcharSpans[index].classList.remove('wrong')
+            }
 
-            // quote.innerHTML = quote.innerHTML.charAt(0)
-       
-        // console.log(quote.innerText[enteredArr.length - 1])
-       
-        quote.innerHTML = quote.innerHTML.replace(quote.innerHTML.charAt(enteredArr.length - 1), quote.innerHTML = `<span id='test' style='color:green;'>${quote.innerHTML.charAt(enteredArr.length - 1)}`)
-            // if(enteredArr[enteredArr.length - 1] === quoteArr[enteredArr.length - 1])
-            // {
-            //     quote.innerHTML = `<span id='test' style='color:green;'>${quote.innerHTML.charAt(enteredArr.length - 1)}</span>${quote.innerHTML.slice(1)}`;
-            //     // quote.innerText[enteredArr.length - 1].style.color = 'green'
-            //     console.log(quote.innerText[enteredArr.length - 1])
-            // }
-        
-        // else
-        // {
-        //     quote.innerHTML = `<span id='test' style='color:red;'>${quote.innerHTML.charAt(enteredArr.length - 1)}</span>${quote.innerHTML.slice(1)}`;
-        //     // quote.innerText[enteredArr.length - 1].style.color = 'green'
-        //     console.log(quote.innerText[enteredArr.length - 1])
-        // }
+            else if (enteredArr[index] === eachSpan.innerText) 
+            {
+                allcharSpans[index].classList.add('correct')
+                allcharSpans[index].classList.remove('wrong')
+            }
+
+            else 
+            {
+                allcharSpans[index].classList.add('wrong')
+                allcharSpans[index].classList.remove('correct')
+            }
+            
+            if(random.length === document.querySelectorAll('.correct').length)
+            {
+                time = -1
+                getQuotes()
+            }
+        })    
     })
-   
-    
-}
 
-function timer()
-{
+function timer() {
     time = 0
     timerEl.innerText = time
-    time = setInterval(() => 
-    {
-        timerEl.innerText = time
+    setInterval(() => {
         time++
+        timerEl.innerText = time
     }, 1000);
 }
-
 
 input.focus()
 
