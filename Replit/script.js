c603411268fbd1dbe801065466b19f71821d913e
Muthyender <n.muthyender99@gmail.com>
2022-04-21 15:12:05 +0530

    practice

--- Replit/script.js
+++ Replit/script.js
@@ -1,40 +1,47 @@
-let btn = document.getElementById('get')
-let users = document.getElementById('users')
-let details = document.getElementById('details')
+// let btn = document.getElementById('get')
+// let users = document.getElementById('users')
+// let details = document.getElementById('details')
 
-let url = 'https://jsonplaceholder.typicode.com/users'
+// let url = 'https://jsonplaceholder.typicode.com/users'
 
-function handleClick()
-{
-    async function getData()
-    {
-        let response = await fetch(url);
-        let data = await response.json()
+// function handleClick()
+// {
+//     async function getData()
+//     {
+//         let response = await fetch(url);
+//         let data = await response.json()
 
-        data.map((eachData, index) =>
-            {
-                users.innerHTML += `<h5 id=${index}>${eachData.email}</h5>` 
-            })
+//         data.map((eachData, index) =>
+//             {
+//                 users.innerHTML += `<h5 id=${index}>${eachData.email}</h5>` 
+//             })
 
-            for(let i=0; i<9; i++)
-            {
-                document.getElementById(`${i}`).addEventListener('mouseover',() =>
-                {
-                    details.innerHTML = `<h5>Name: ${data[i].name}</h5> <h5> Email: ${data[i].email} </h5>  <h5>Username: ${data[i].username}</h5> <button id='posts'>Show Posts</button>`
+//             for(let i=0; i<9; i++)
+//             {
+//                 document.getElementById(`${i}`).addEventListener('mouseover',() =>
+//                 {
+//                     details.innerHTML = `<h5>Name: ${data[i].name}</h5> <h5> Email: ${data[i].email} </h5>  <h5>Username: ${data[i].username}</h5> <button id='posts'>Show Posts</button>`
 
-                    document.getElementById('posts').addEventListener('click', () =>
-                    {
-                        details.innerHTML += `<h5>Company: ${Object.entries(data[i].company)}</h5>`
-                    })
-                } ) 
-            }
-    }
-    getData()
-}
+//                     document.getElementById('posts').addEventListener('click', () =>
+//                     {
+//                         details.innerHTML += `<h5>Company: ${Object.entries(data[i].company)}</h5>`
+//                     })
+//                 } ) 
+//             }
+//     }
+//     getData()
+// }
 
-console.log(x)
-var x =12;
+// console.log(x)
+// var x =12;
     
 
 
-btn.addEventListener('click', handleClick)
+// btn.addEventListener('click', handleClick)
+
+
+
+let a = '99999999999999999999'
+let b = '99999999999999999999'
+
+console.log(a+b);
